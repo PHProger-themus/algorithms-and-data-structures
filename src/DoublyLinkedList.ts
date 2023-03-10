6bb8d44a24e85b10a9151aa04fe4884815ec0b66
boguchar <bogdan.raguzin@gmail.com>
2023-03-10 23:15:59 +0300

    feat: doubly linked list, fix: refactored linked list

--- src/DoublyLinkedList.ts
+++ src/DoublyLinkedList.ts
@@ -1,6 +1,13 @@
-class DoublyListNode<T> {
-    public next: DoublyListNode<T> | null = null
-    public prev: DoublyListNode<T> | null = null
+// Это двусвязный список. Перед его изучением ознакомьтесь с реализацией односвязного списка, если вы еще этого не сделали.
+
+// Парочка типов, чтобы нам было комфортнее
+type ListNodeOrNull<T> = ListNode<T> | null
+type Bounds<T> = { head: ListNode<T>, tail: ListNode<T> } | null
+
+class ListNode<T> {
+    public next: ListNodeOrNull<T> = null
+    // Нода теперь будет хранить ссылку еще и на предыдущий элемент
+    public prev: ListNodeOrNull<T> = null
     public data: T
 
     constructor(data: T) {
@@ -8,41 +15,62 @@ class DoublyListNode<T> {
     }
 }
 
-class DoublyList<T> {
-    private head: DoublyListNode<T> | null = null
+// Реализовывая списки на высокоуровневых языках, можно делать с ними все, что угодно (например извращаться с методом удаления, что я и сделал).
+// Вряд ли такое задумывалось изначально...
+class List<T> {
+    // А давайте будем хранить также ссылочку и на "хвост" списка - последний элемент
+    // head и tail должны быть в согласованности, они либо оба null, либо оба имеют значения. Об этом можно забыть.
+    // Поэтому я ввожу специальный тип Bounds, который следит за этим.
+    private bounds: Bounds<T> = null
 
-    public prepend(node: DoublyListNode<T>): void {
-        const prevHead = this.head
-        this.head = node
-        if (prevHead) {
-            this.head.next = prevHead
-            prevHead.prev = this.head
-        }
-    }
-
-    public append(node: DoublyListNode<T>): void {
-        if (!this.head) {
-            this.head = node
+    // Если bounds не заданы, значит мы добавляем первый элемент. Иначе - смотрим, какой элемент был первым,
+    // и делаем его вторым, не забываем поменять обе ссылки
+    // Я передаю в функцию ноду, а не данные, которые она должна хранить. Это также не принципиально,
+    // можно создавать ноду в методе, передавая ей в конструктор данные
+    public prepend(node: ListNode<T>): void {
+        if (!this.bounds) {
+            this.bounds = { head: node, tail: node }
         } else {
-            const lastNode = this.find(n => !n.next)!
-            lastNode.next = node
-            node.prev = lastNode
+            this.bounds.head.prev = node
+            node.next = this.bounds.head
+            this.bounds.head = node
         }
     }
 
-// Доработать, узел имеет ссылки в обе стороны и не нужен этот бред
-    public delete(node: DoublyListNode<T>): void {
-        const prevNode = this.find(n => n.next === node)
-        if (prevNode) {
-            prevNode.next = node.next
-            if (node.next) {
-                node.next.prev = prevNode
-            }
+    // Аналогично методу выше, только все наоборот
+    public append(node: ListNode<T>): void {
+        if (!this.bounds) {
+            this.bounds = { head: node, tail: node }
+        } else {
+            this.bounds.tail.next = node
+            node.prev = this.bounds.tail
+            this.bounds.tail = node
         }
+    }
+
+    // Удаление по условию. Находим элемент и меняем ссылки его соседних элементов, теперь они тупо игнорят его :)
+    // Можно также передать просто ноду
+    public delete(nodeOrCallback: ListNode<T> | ((node: ListNode<T>) => boolean)): void {
+        const node = nodeOrCallback instanceof ListNode ? nodeOrCallback : this.find(nodeOrCallback)
+        if (node) {
+            node.prev && (node.prev.next = node.next)
+            node.next && (node.next.prev = node.prev)
+        }
+    }
+
+    // Я нарочно не реализовываю возврат массива с нодами, так как произойдет закольцевание ссылок. Но реализовать можно, это не сложно.
+    public visualize(): string {
+        const nodes: T[] = []
+        let currNode = this.getHead()
+        while (currNode) {
+            nodes.push(currNode.data)
+            currNode = currNode.next
+        }
+        return nodes.join(' <-> ')
     }
 
     public size(): number {
-        let currNode: DoublyListNode<T> | null = this.head
+        let currNode = this.getHead()
         let size = 0
         while (currNode) {
             size++
@@ -51,8 +79,8 @@ class DoublyList<T> {
         return size
     }
 
-    public find(findCallback: (node: DoublyListNode<T>) => boolean): DoublyListNode<T> | null {
-        let currNode: DoublyListNode<T> | null = this.head
+    public find(findCallback: (node: ListNode<T>) => boolean): ListNodeOrNull<T> {
+        let currNode = this.getHead()
         while (currNode) {
             if (findCallback(currNode)) {
                 return currNode
@@ -62,32 +90,22 @@ class DoublyList<T> {
         return null
     }
 
-    public toArray(): T[] {
-        const nodes: T[] = []
-        let currNode: DoublyListNode<T> | null = this.head
-        while (currNode) {
-            nodes.push(currNode.data)
-            currNode = currNode.next
-        }
-        return nodes
-    }
-
-    public toString(): string {
-        return this.toArray().join(' -> ')
+    // С новым свойством bounds операция теперь немного усложнилась
+    private getHead() {
+        return this.bounds?.head || null
     }
 }
 
-let doublyList = new DoublyList<number>()
-const doublyNodeThird = new DoublyListNode(3)
+const list = new List<number>()
 
-doublyList.append(new DoublyListNode(1))
-doublyList.append(new DoublyListNode(2))
-doublyList.prepend(doublyNodeThird)
-doublyList.append(new DoublyListNode(4))
-doublyList.prepend(new DoublyListNode(5))
+list.append(new ListNode(1))
+list.append(new ListNode(2))
+list.append(new ListNode(3))
+list.append(new ListNode(4))
+list.append(new ListNode(5))
 
-console.log(doublyList.toArray())
+console.log(list.visualize()) // 1 <-> 2 <-> 3 <-> 4 <-> 5
 
-doublyList.delete(doublyNodeThird)
+list.delete(node => node.data === 2)
 
-console.log(doublyList.toArray())
+console.log(list.visualize()) // 1 <-> 3 <-> 4 <-> 5
--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -1,5 +1,7 @@
+type ListNodeOrNull<T> = ListNode<T> | null
+
 class ListNode<T> {
-    public next: ListNode<T> | null = null
+    public next: ListNodeOrNull<T> = null
     public data: T
 
     constructor(data: T) {
@@ -7,17 +9,22 @@ class ListNode<T> {
     }
 }
 
+// Реализовывая списки на высокоуровневых языках, можно делать с ними все, что угодно (например извращаться с методом удаления, что я и сделал).
+// Вряд ли такое задумывалось изначально...
 class List<T> {
-    private head: ListNode<T> | null = null
+    private head: ListNodeOrNull<T> = null
 
+    // Просто добавим элемент в начало, то есть сделаем его головой, и ссылку next зададим как предыдущая голова
     public prepend(node: ListNode<T>): void {
-        const prevHead = this.head
-        this.head = node
-        if (prevHead) {
-            this.head.next = prevHead
+        if (!this.head) {
+            this.head = node
+        } else {
+            node.next = this.head
+            this.head = node
         }
     }
 
+    // append не подразумевается для односвязного списка, т.к. это крайне неэффективная операция, нужно итерироваться, чтобы найти ласт ноду
     public append(node: ListNode<T>): void {
         if (!this.head) {
             this.head = node
@@ -27,16 +34,31 @@ class List<T> {
         }
     }
 
-    public delete(node: ListNode<T>): void {
-        const prevNode = this.find(n => n.next === node)
-        if (prevNode) {
-            prevNode.next = node.next
+    // Усложнился за счет callback на один уровень вложенности (зачем я его вообще сюда впиндюрил?)
+    public delete(nodeOrCallback: ListNode<T> | ((node: ListNode<T>) => boolean)): void {
+        if (this.head) {
+            const node = nodeOrCallback instanceof ListNode ? nodeOrCallback : this.find(nodeOrCallback)
+            if (node) {
+                // Если первая нода - просто ставим следующую как голову (или там будет null, если нода одна в списке, что значит, что он очистится)
+                if (this.head === node) {
+                    this.head = this.head!.next
+                } else {
+                    // Иначе - ищем предыдущую ноду и меняем ей next.
+                    const prevNode = this.find(n => n.next === node)
+                    if (prevNode) {
+                        prevNode.next = node.next
+                        // Удаление ноды должно сопровождаться удалением ее из памяти, но у нас тут высокий уровень, нам можно и так)
+                        // На самом деле в TS/JS сборка мусора осуществится автоматически, но на низких уровнях здесь должна быть операция удаления ноды из памяти
+                    }
+                }
+            } 
         }
     }
 
+    // Просто выведу список в виде строки "нода -> нода -> нода". Для дебага.
     public visualize(): string {
         const nodes: T[] = []
-        let currNode: ListNode<T> | null = this.head
+        let currNode = this.head
         while (currNode) {
             nodes.push(currNode.data)
             currNode = currNode.next
@@ -45,7 +67,7 @@ class List<T> {
     }
 
     public size(): number {
-        let currNode: ListNode<T> | null = this.head
+        let currNode = this.head
         let size = 0
         while (currNode) {
             size++
@@ -54,8 +76,9 @@ class List<T> {
         return size
     }
 
-    public find(findCallback: (node: ListNode<T>) => boolean): ListNode<T> | null {
-        let currNode: ListNode<T> | null = this.head
+    // Поиск при помощи callback. Возможна реализация через поиск по data. Но мне, например, пригодилась данная реализация для создания метода append.
+    public find(findCallback: (node: ListNode<T>) => boolean): ListNodeOrNull<T> {
+        let currNode = this.head
         while (currNode) {
             if (findCallback(currNode)) {
                 return currNode
